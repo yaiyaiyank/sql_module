5319e1a839c2c0b7c1e1a6bf307aa0fd37df8ecf
yaiyaiyank <228066892+yaiyaiyank@users.noreply.github.com>
2025-10-14 12:37:18 +0900

    query_builder.py の作成

---
+++ sql_module/sqlite/table/insert/query_builder.py
@@ -0,0 +1,3 @@
+from yai.entry import Log, dataclass, field, Path, datetime, base_utils_module
+from yai.entry.sql_module.exceptions import ConstraintConflictError
+from ..column.column import Column, ColumnConstraint
