85ea33330bb7385246b59ded01d6c7f516b55dee
yaiyaiyank <228066892+yaiyaiyank@users.noreply.github.com>
2025-10-14 12:46:18 +0900

    query_builder.py の更新

--- sql_module/sqlite/table/insert/query_builder.py
+++ sql_module/sqlite/table/insert/query_builder.py
@@ -1,3 +1,14 @@
 from yai.entry import Log, dataclass, field, Path, datetime, base_utils_module
 from yai.entry.sql_module.exceptions import ConstraintConflictError
 from ..column.column import Column, ColumnConstraint
+
+@dataclass
+class InsertQueryBuilder:
+    log: Log = field(default_factory=Log)
+
+    @staticmethod
+    def get_head_query(exists_ok: bool) -> str:
+        """最初のクエリ作成"""
+        if exists_ok:
+            return "INSERT"
+        return "INSERT"
